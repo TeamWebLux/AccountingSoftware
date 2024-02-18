40a2b322188048dae6a6cae3b95de9785b877651
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-02-18 22:00:12 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',function (){
+Route::HEAD('/',function (){
     return view('Templete');
 })->name('Templete');
 Route::get('/m',function (){
